683ff7c51f30a198d0a8f05ac183066a5d59b905
Dan <lyndt234@gmail.com>
2018-05-18 00:00:13 +0700

    Add images to database

---
+++ MySQL_Scripts/script.sql
@@ -0,0 +1,9 @@
+source first.sql;
+source user.sql;
+source carrenter.sql;
+source car.sql;
+source listing.sql;
+source availability.sql;
+/ * source booking.sql; */
+source transaction.sql;
+source review.sql;
